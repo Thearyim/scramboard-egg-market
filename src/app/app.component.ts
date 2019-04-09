af25346e0b3fcdad8c6f3131c9308e7e3ea6b0a8
Theary Im and Young Liu <young@>
2019-04-09 10:32:47 -0700

    add a marketplace and about page with routing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Video Game Marketplace';
+  title = 'Scramboard Eggs Marketplace';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from "./app.routing";
+import { AboutComponent } from './about/about.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    AboutComponent,
+    MarketplaceComponent
   ],
   imports: [
     BrowserModule,
